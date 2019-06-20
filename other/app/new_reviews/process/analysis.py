f506215857fee8bb8d4e14662ac7c745cc7a8d59
unknown <879833043@qq.com>
2019-06-20 10:11:50 +0800

    other py

--- other/app/new_reviews/process/analysis.py
+++ other/app/new_reviews/process/analysis.py
@@ -361,7 +361,7 @@ class OpinionExtraction(threading.Thread):
         # self.opinions = lexicon.opinions
         # self.O_Seed = self.opinions
         # self.O_Map = {}
-        self.T_Seed = get_target_seed(self.pcid, self.cid)
+        self.T_Seed = get_target_seed(self.pcid, self.cid, self.is_add)
 
         self.remove()
         self.get_ml_text()
